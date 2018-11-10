df8eee255aad4c93dc7b0bbfa5daffbfc9448e8a
Nathen Lyman <nplym@protonmail.com>
2018-11-10 13:59:28 -0500

    Setup angular bootstrap

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleScreenComponent } from './title-screen/title-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleScreenComponent
   ],
   imports: [
     BrowserModule,
