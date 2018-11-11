b43c94ec2ad597987dcad03f224522bfcebbf7b7
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2018-11-11 06:39:21 -0500

    [ILWHackCincy2018-37] Load Meetups

--- client/src/app/raid-description/raid-description.component.ts
+++ client/src/app/raid-description/raid-description.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {MeatUp} from '../models/meatup.model';
 
 @Component({
   selector: 'app-raid-description',
@@ -7,6 +8,7 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RaidDescriptionComponent implements OnInit {
 
+@Input() meatup: MeatUp;
 
   constructor() {
   }
