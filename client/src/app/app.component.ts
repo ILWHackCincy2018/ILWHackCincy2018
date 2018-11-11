4637ff47109edd9db902ffd6877f378036e3b8e8
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2018-11-11 09:51:47 -0500

    [ILWHackCincy2018-37] Load Meetups

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
-import {StateService} from '@uirouter/angular';
 import {AngularFireAuth} from '@angular/fire/auth';
 import {AngularFirestore} from '@angular/fire/firestore';
+import {StateService} from '@uirouter/angular';
 import * as firebase from 'firebase/app';
 import {Observable} from 'rxjs';
 
@@ -15,12 +15,17 @@ export class AppComponent {
 
   user: firebase.User;
   items: Observable<any[]>;
+  myRaids: Observable<any[]>;
   constructor(public afAuth: AngularFireAuth,
-    db: AngularFirestore,
+    private db: AngularFirestore,
     public state: StateService) {
     this.user = firebase.auth().currentUser;
 
     this.items = db.collection('RoadKill').valueChanges();
+  }
+
+  gotClicked(){
+    this.myRaids = this.db.collection('users').doc(this.user.uid).collection('meetups').valueChanges();
   }
 
   login() {
