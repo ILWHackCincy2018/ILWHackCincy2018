97bec3144e6e8986b42b6c4c500f501ab50f73e3
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2018-11-11 09:33:55 -0500

    [ILWHackCincy2018-37] Load Meetups

--- client/src/app/create-raid/create-raid.component.ts
+++ client/src/app/create-raid/create-raid.component.ts
@@ -4,8 +4,6 @@ import {AngularFirestore} from '@angular/fire/firestore';
 import * as firebase from 'firebase/app';
 import {MeatUp} from '../models/meatup.model';
 import {MapsService} from '../services/maps.service';
-import {MeatUp} from '../models/meatup.model';
-import {Role} from '../models/role.model';
 
 @Component({
   selector: 'app-create-raid',
