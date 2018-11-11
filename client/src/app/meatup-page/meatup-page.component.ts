a7895f08a777a7b7adef6eb43c27084da59137df
mbuda <michael.buda@ilwllc.com>
2018-11-11 08:43:42 -0500

    partial (?) fix for unpopulated maps under meatup page

--- client/src/app/meatup-page/meatup-page.component.ts
+++ client/src/app/meatup-page/meatup-page.component.ts
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeatupPageComponent implements OnInit {
 
-  constructor() { }
+  items: Observable<any[]>;
+  constructor(db: AngularFirestore)) { 
+    this.items = db.collection('RoadKill').valueChanges();
+  }
 
   ngOnInit() {
   }
